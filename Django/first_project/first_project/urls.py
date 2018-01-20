bcb57f4dcfda93aedb8a399f3e8bb07d85771609
hono63 <hono6363@yahoo.co.jp>
2018-01-20 22:48:43 +0900

    url追加忘れ

--- Django/first_project/first_project/urls.py
+++ Django/first_project/first_project/urls.py
@@ -22,3 +22,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('worker_list/', manager_view.WorkerListView.as_view()),
 ]
+
+from django.conf import settings
+from django.conf.urls import include, url
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]

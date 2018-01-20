c8a6068ca990bfd658fb94c0cd02c78b64d1d78a
hono63 <hono6363@yahoo.co.jp>
2018-01-20 08:19:04 +0000

    Django 追加

---
+++ Django/first_project/manager/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from manager.models import *
+
+# Create your views here.
+#get_object_404(Person, id=20)
+
+class WorkerListView(TemplateView):
+    template_name = "worker_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+
+        workers = Worker.objects.all()
+        context ['workers'] = workers
+
+        return render(self.request, self.template_name, context)

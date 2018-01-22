ad2c0cf6f81cd9f8946ef149694f9b6d5a75b7c8
hono63 <hono6363@yahoo.co.jp>
2018-01-22 22:00:08 +0900

    admine追加

--- Django/first_project/first_project/settings.py
+++ Django/first_project/first_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrapform', # デバッグツール
+    'bootstrapform', # フォーム
     'debug_toolbar', # デバッグツール
     'manager',  #追加部分
 ]
